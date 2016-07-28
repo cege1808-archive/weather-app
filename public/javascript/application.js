814d730c5ef559226b58e022ded21aaa7b179236
cege1808 <gunawan.clarissa@hotmail.com>
2016-07-28 21:29:47 +0000

    display result

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -15,23 +15,54 @@ $(document).ready(function() {
         success: function(data){
           result = data.RESULTS;
           $(".searchList").children().remove();
-          // console.log(data.RESULTS);
+          // console.log(result);
           $.each(result, function(index, res){
-            console.log(res.name);
-            $(".searchList").append("<p><span>"+ res.name +"</span></p>");
+            // console.log(res.name);
+            // console.log(res);
+            $(".searchList").append("<p><span data-query='" + res.zmw +"'>"+ res.name +"</span></p>");
           })
         }
       });
     }
   }
 
+  function getCondition(query){
+    $.ajax({
+      url: 'http://api.wunderground.com/api/500a16a262073dee/conditions/q/zmw:' + query + '.json',
+      method: 'GET',
+      jsonp: 'callback',
+      dataType: 'jsonp',
+      success: function(data){
+        console.log(data);
+        displayCondition(data.current_observation);
+      }
+    });
+  }
+
+  function displayCondition(observation){
+    console.log(observation.weather);
+    console.log(observation.temp_c);
+    console.log(observation.icon_url);
+
+    var weather = "<p>" + observation.weather + "</p>";
+    var temperatureC = "<p>" + observation.temp_c + "</p>";
+    var icon = "<img src='" + observation.icon_url + "'/>";
+    var $display = $(".displayWeather");
+
+    $display.append(weather);
+    $display.append(temperatureC);
+    $display.append(icon);
+  }
+
+
   $("input").on('keyup', function(){
     autocomplete();
   });
 
   $(".searchList").on('click', 'span', function(){
-    var query = $(this).text();
+    var query = $(this).data("query");
     console.log("clicked " + query);
+    getCondition(query);
   });
 
 });
