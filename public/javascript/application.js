58d54435d880f8cfd1acc783ea588a2970bd1213
cege1808 <gunawan.clarissa@hotmail.com>
2016-07-28 16:13:30 +0000

    basic autocomplete feature

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,36 @@
 $(document).ready(function() {
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  function autocomplete(){
+    keyword = $("input").val();
+    console.log(keyword);
+    if (keyword == ""){
+      $(".display").children().remove();
+    }
+    else {
+      $.ajax({
+        url: 'http://autocomplete.wunderground.com/aq?query=' + keyword,
+        method: 'GET',
+        jsonp: 'cb',
+        dataType: 'jsonp',
+        success: function(data){
+          result = data.RESULTS;
+          $(".display").children().remove();
+          // console.log(data.RESULTS);
+          $.each(result, function(index, res){
+            console.log(res.name);
+            $(".display").append("<p>"+ res.name +"</p>");
+          })
+        }
+      });
+    }
+  }
+
+  $("input").on('keyup', function(){
+    autocomplete();
+  });
+
+  $("display").on('click', 'p', function(){
+    console.log("clicked city");
+  });
+
 });
